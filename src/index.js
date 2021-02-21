9462ef90cfef4bdc74e094470cd5ca6731257bad
KaiYun Cheng <tanyacheng@gmail.com>
2021-02-22 07:44:30 +0800

    feat: slider section

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './styles.scss';
+import '@fortawesome/fontawesome-free/css/fontawesome.css';
+import '@fortawesome/fontawesome-free/css/brands.css';
+import '@fortawesome/fontawesome-free/css/solid.css';
+console.log("hello world!");
